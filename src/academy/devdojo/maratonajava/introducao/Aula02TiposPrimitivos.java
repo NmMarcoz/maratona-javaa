244eab9d072ba2ab28f1e99cb627f29dff8616bd
NmMarcoz <marcosrafael.nm62@gmail.com>
2023-09-14 20:53:30 -0300

    aula2

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -5,8 +5,15 @@ public class Aula02TiposPrimitivos {
         //int, double, float, char, byte, short, long, boolean
         //tipos primitivos como os de cima são escritos em letras minúsculas!
         //para criar uma variável, primeiro o tipo depois o nome!
-        int idadeDoPai;
-
+        int idadeDoPai = 10;
+        long numeroGrande = 100000990;
+        double salarioDouble = 2000;
+        float salarioFloat = 2.50F;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'k';
 
 
     }
