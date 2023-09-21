bd65f1f8cb7a38af283fd88a11e1de8a488198c5
NmMarcoz <marcosrafael.nm62@gmail.com>
2023-09-21 19:24:13 -0300

    case.2

--- src/academy/devdojo/maratonajava/introducao/switchexample.java
+++ src/academy/devdojo/maratonajava/introducao/switchexample.java
@@ -1,13 +1,13 @@
 package academy.devdojo.maratonajava.introducao;
-
+//control alt L para identar!
 public class switchexample {
     public static void main(String[] args) {
-         //imprimindo o dia da semana considerando 1 como domingo
+        //imprimindo o dia da semana considerando 1 como domingo
         byte dia = 5;
-        switch (dia){
+        switch (dia) {
             case 1:
                 System.out.println("domingo!");
-                break;
+                break; //Sem esse break aqui o switch ia imprimir os demais casos seguintes.
             case 2:
                 System.out.println("segunda!");
                 break;
@@ -26,7 +26,22 @@ public class switchexample {
             case 7:
                 System.out.println("sábado!");
                 break;
+            default: //Se nenhum dos casos for verdadeiro, esta é a opção que vai ser utilizada!
+                System.out.println("opção inválida!");
+                break;
         }
 
+        char sexo = 'm';
+        switch(sexo){
+            case 'm':
+                System.out.println("masculino!");
+                break;
+            case 'f':
+                System.out.println("feminino!");
+                break;
+            default:
+                System.out.println("inválido!");
+        }
+        
     }
 }
