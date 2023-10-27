5266da33a5ca0140ade796d6473fe400a363282a
NmMarcoz <marcosrafael.nm62@gmail.com>
2023-10-27 00:50:41 -0300

    Complementares da primeira aula: Padroes de inicalização

--- src/academy/devdojo/maratonajava/javacore/introducaoclasses/domain/Estudante.java
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/domain/Estudante.java
@@ -1,7 +1,7 @@
 package academy.devdojo.maratonajava.javacore.introducaoclasses.domain;
 
 public class Estudante {
-    public String nome;
+    public String nome = "Gokudadeimon";
     public int idade;
     public char sexo;
 
--- src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest.java
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest.java
@@ -9,8 +9,19 @@ public class EstudanteTest {
         estudante.nome = "Malaquias";
         estudante.sexo = 'M';
 
+        Estudante estudante2 = new Estudante();
+        estudante2.idade = 999;
+        estudante2.sexo =  'M';
+
+
         System.out.println("Nome do estudante é: " +estudante.nome);
         System.out.println("Idade do estudante é: " +estudante.idade);
         System.out.println("Sexo do estudante é: " +estudante.sexo);
+
+        System.out.println("---------------------");
+
+        System.out.println(estudante2.nome); // Como lá na classe estudante eu defini que nome possui um valor de inicialização e neste objeto eu não defini nada para o nome, esse atributo assumiu o valord e inciialização.
+        System.out.println(estudante2.idade);
+        System.out.println(estudante2.sexo);
     }
 }
