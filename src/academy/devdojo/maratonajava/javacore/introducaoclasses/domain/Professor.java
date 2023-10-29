0553ce2a75683bd0039a6439091b49cf29347f49
NmMarcoz <marcosrafael.nm62@gmail.com>
2023-10-28 22:32:22 -0300

    Aula 41 - Coesão

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/domain/Professor.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.domain;
+
+public class Professor {
+    public String nome;
+    public String materia;
+    public int idade;
+    public char sexo;
+
+    public void Console(){
+        System.out.println("Nome do professor é: " + this.nome);
+        System.out.println("Idade do professor é: " + this.idade);
+        System.out.println("Matéria do professor é: " + this.materia);
+        System.out.println("Sexo do professor é: " + this.sexo);
+
+
+
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/ProfessorTest.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.domain.Professor;
+
+public class ProfessorTest {
+    public static void main(String[] args) {
+        Professor professor01 = new Professor();
+        professor01.nome = "Jonas";
+        professor01.materia = "Portugues";
+        professor01.idade = 25;
+        professor01.sexo = 'M';
+
+        professor01.Console();
+    }
+}
