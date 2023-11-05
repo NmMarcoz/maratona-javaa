318c1555b0241acfae0ce2caf61104f4303aaa9f
NmMarcoz <marcosrafael.nm62@gmail.com>
2023-11-05 00:01:05 -0300

    Métodos estáticos | Aula 62

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatic/domain/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatic/domain/Carro.java
@@ -3,40 +3,47 @@ package academy.devdojo.maratonajava.javacore.Fmodificadorestatic.domain;
 public class Carro {
     private String name;
     private double maxVelo;
-    public static double limVelo = 250;
+   private static double limVelo = 250;
 
     public Carro(String name, double maxVelo) {
         this.name = name;
         this.maxVelo = maxVelo;
     }
 
-    public void console(){
-        System.out.println("Nome do carro é: "+ this.name);
-        System.out.println("Velocidade máxima: "+ this.maxVelo);
-        if(limVelo!= 0.0){
-            System.out.println("Limite de velocidade: "+ Carro.limVelo);
+    public void console() {
+        System.out.println("Nome do carro é: " + this.name);
+        System.out.println("Velocidade máxima: " + this.maxVelo);
+        if (limVelo != 0.0) {
+            System.out.println("Limite de velocidade: " + Carro.limVelo);
         }
 
     }
 
-    public String getName(){
-        return this.name;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public double getMaxVelo(){
-        return this.maxVelo;
-    }
-    public void setMaxVelo(double maxVelo){
-        this.maxVelo = maxVelo;
-    }
-    public double getLimVelo(){
-        return Carro.limVelo;
-    }
-    public void setLimVelo(double limVelo){
+    public static void setLimVelo(double limVelo) {
         Carro.limVelo = limVelo;
     }
 
+    public static double getLimVelo() {
+        return Carro.limVelo;
+    }
+
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMaxVelo() {
+        return this.maxVelo;
+    }
+
+    public void setMaxVelo(double maxVelo) {
+        this.maxVelo = maxVelo;
+    }
 }
+
+
--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatic/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatic/test/CarroTest01.java
@@ -7,7 +7,9 @@ public class CarroTest01 {
         Carro c1 = new Carro("Porsche 311 GT3s", 225);
         Carro c2 = new Carro("Toyota Trueno AE86", 120);
         Carro c3 = new Carro("Porsche Cayman", 225);
-        Carro.limVelo = 190; //Por se tratar de um modificador static ele afeta a classe em si, não só o objeto. Ah, ele é public tb
+        Carro.setLimVelo(210); //Por se tratar de um modificador static ele afeta a classe em si, não só o objeto. Ah, ele é public tb
+        System.out.println("Velocidade limite depois da alteração é: " + Carro.getLimVelo());
+        System.out.println("--------");
         c1.console();
         System.out.println("--------");
         c2.console();
