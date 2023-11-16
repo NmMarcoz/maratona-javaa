d8ff3f27ba8fb17b1bf1ad431f8807b8ae41a478
NmMarcoz <marcosrafael.nm62@gmail.com>
2023-11-16 00:42:03 -0300

    Jogando novas exceções com THROW | Aula 98

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExcpetionTest02.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExcpetionTest02 {
+    public static void main(String[] args) {
+        System.out.println(divisao(4,0));
+    }
+    private static int divisao(int a, int b){
+        if( b == 0){
+            throw new IllegalArgumentException("Argumento ilegal. A divisão não pode ser feita por zero."); // Joga o erro pro usuário e permite não precisar de um return 0.
+        }
+        return a/b;
+    }
+}
