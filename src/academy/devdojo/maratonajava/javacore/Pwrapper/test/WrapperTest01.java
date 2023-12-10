cc547b3dc030e8da763234ba07b049a7af70d7c5
NmMarcoz <marcosrafael.nm62@gmail.com>
2023-12-10 16:21:33 -0300

    Classes Utilitárias - Wrappers pt 02

--- src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -16,12 +16,17 @@ public class WrapperTest01 {
 
         Byte byteW= 1;
         Short shortW= 1;
-        Integer intW= 1;
+        Integer intW= 1; // Isso aqui é autoboxing.
         Long longW= 1L;
         Float floatW= 11.4F;
         Double doubleW= 245.5D;
 
         Character charW= 'w';
         Boolean booleanW= false;
+
+        int  i = intW; //O nome disso aqui é Unboxing
+        Integer intW2 = Integer.parseInt("1"); // Transformando string em inteiro por método do wrapper.
+        Integer intW3 = Integer.parseInt("2");
+
     }
 }
