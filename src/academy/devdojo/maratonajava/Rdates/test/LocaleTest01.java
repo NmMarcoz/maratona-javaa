42ab42144c1dba18e41dfa24a779efd9b8868aa4
NmMarcoz <marcosrafael.nm62@gmail.com>
2023-12-11 00:30:34 -0300

    Classes Utilitárias - Locale

---
+++ src/academy/devdojo/maratonajava/Rdates/test/LocaleTest01.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.Rdates.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        Locale localeItaly = new Locale("it", "IT");
+        Locale localeCh = new Locale("it", "CH");
+        Locale localeJp = new Locale("ja", "JP");
+
+        Calendar calendar = Calendar.getInstance();
+
+        DateFormat df = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localeCh);
+        DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL, localeJp);
+
+        System.out.println("Data italia : " + df.format(calendar.getTime()));
+        System.out.println("Data Suíça : " + df2.format(calendar.getTime()));
+        System.out.println("Data Japão : " + df3.format(calendar.getTime()));
+
+        System.out.println(localeItaly.getDisplayCountry(localeJp));
+        System.out.println(localeCh.getDisplayCountry(localeJp));
+        System.out.println(localeJp.getDisplayCountry(localeJp));
+
+    }
+}
