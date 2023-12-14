4327d9d266abe50e7da74f70ae0d078c507c57de
NmMarcoz <marcosrafael.nm62@gmail.com>
2023-12-14 20:08:29 -0300

    Coleções pt 04 - Hashcode pt 02

--- src/academy/devdojo/maratonajava/javacore/Ycollections/domain/Smartphone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycollections/domain/Smartphone.java
@@ -4,7 +4,7 @@ public class Smartphone {
     private String serialNumber;
     private String marca;
 
-    public Smartphone(String serialNumber, String marca){
+    public Smartphone(String serialNumber, String marca) {
         this.serialNumber = serialNumber;
         this.marca = marca;
     }
@@ -24,5 +24,18 @@ public class Smartphone {
     public void setMarca(String marca) {
         this.marca = marca;
     }
-    
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == null) return false;
+        if(this == obj) return true;
+        if(this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return serialNumber != null && serialNumber.equals(smartphone.getSerialNumber());
+    }
+
+    @Override
+    public int hashCode(){
+             return serialNumber == null? 0: this.serialNumber.hashCode();
+    }
 }
