43209a84a032a402af6f046fd617a5b15100a477
NmMarcoz <marcosrafael.nm62@gmail.com>
2023-12-17 22:43:46 -0300

    Coleções pt 13 - Conversão de Lista para Arrays e vice-versa

--- src/academy/devdojo/maratonajava/javacore/Ycollections/test/MangasSortTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycollections/test/MangasSortTest01.java
@@ -4,7 +4,15 @@ import academy.devdojo.maratonajava.javacore.Ycollections.domain.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class sorteMangaById implements Comparator<Manga>{
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return o1.getId().compareTo(o2.getId());
+    }
+}
 
 public class MangasSortTest01 {
     public static void main(String[] args) {
@@ -15,17 +23,18 @@ public class MangasSortTest01 {
         mangas.add(new Manga(9L, "Banana fish", 39.20));
         mangas.add(new Manga(12L, "Btooom", 49.80));
         mangas.add(new Manga(12L, "Btooom", 49.80));
+        mangas.add(new Manga(13L, "Bokan", 49.80)); //
 
         for (Manga mg : mangas) {
             System.out.println(mg.getId());
             System.out.println("-----");
         }
         System.out.println("------------------SORTING------------------");
-        Collections.sort(mangas);
+        Collections.sort(mangas, new sorteMangaById());
         for (Manga mg : mangas) {
             System.out.println(mg.getId());
             System.out.println("----");
         }
-        //System.out.println(mangas.get(5).getId().equals((mangas.get(4).getId())));
+         //System.out.println(mangas.get(5).getId().equals((mangas.get(4).getId())));
     }
 }
